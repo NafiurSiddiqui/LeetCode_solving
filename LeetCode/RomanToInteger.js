0fca3fd57027c841f7d84cefd1c6298b3b61a0b0
saejim <saejim91@gmail.com>
2022-09-06 04:35:14 +0600

    Variations

--- LeetCode/RomanToInteger.js
+++ LeetCode/RomanToInteger.js
@@ -1,5 +1,4 @@
 var romanToInt = function (s) {
-
 	//roman symbols and value
 
 	const romanSymbols = {
@@ -12,33 +11,30 @@ var romanToInt = function (s) {
 		M: 1000,
 	};
 
-
 	let newInput = s.split('');
 	let result = 0;
 
 	//forEach
-	// newInput.forEach((el, index) => {
+	newInput.forEach((el, index) => {
 
-	// 			if (romanSymbols[el] < romanSymbols[newInput[index + 1]]) result -= romanSymbols[el];
-	// 			else result += romanSymbols[el];
+				// if (romanSymbols[el] < romanSymbols[newInput[index + 1]]) result -= romanSymbols[el];
+				// else result += romanSymbols[el];
 
-	// 	});
-	
-		//traditional loop	
-		for (let i = 0; i < newInput.length; i++){
+				romanSymbols[el] < romanSymbols[newInput[index + 1]] ?  result -= romanSymbols[el] : result += romanSymbols[el];
 
-			let curInput = romanSymbols[newInput[i]];
-			let nextInput = romanSymbols[newInput[i + 1]];
+		});
 
-			curInput < nextInput ? result -= romanSymbols[newInput[i]]: result += romanSymbols[newInput[i]];
+	//traditional loop
+	// for (let i = 0; i < newInput.length; i++) {
+	// 	let curInput = romanSymbols[newInput[i]];
+	// 	let nextInput = romanSymbols[newInput[i + 1]];
 
+	// 	// curInput < nextInput ? result -= romanSymbols[newInput[i]]: result += romanSymbols[newInput[i]];
+	// 	if (curInput < nextInput) result -= romanSymbols[newInput[i]];
+	// 	else result += romanSymbols[newInput[i]];
+	// }
 
-		}
-
-		console.log(result);
-
+	console.log(result);
 };
 
-romanToInt('LVIII');
-
-
+romanToInt('IV');
